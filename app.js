f8c0f12ba99d16bc8079a4a8f2de4be8782bfb1b
Satssuki <danielarya185@gmail.com>
2020-06-12 22:22:46 +0900

    the validateS and createObject function has finished

---
+++ app.js
@@ -0,0 +1,27 @@
+function main() {
+    const readline = require('readline-sync').question;
+
+    let playerObject = createObject(1, 0);
+    let S = validateS(readline("Masukan S: "));
+}
+
+function validateS(S = String()) {
+    let parsed = Number.parseInt(S);
+    let result = -1;
+    if(!Number.isNaN(parsed)) {
+        let n = (parsed + 1) / 4;
+        if(Number.isInteger(n) && n >= 3) {
+            result = parsed;
+        }   
+    }
+    return result;
+}
+
+function createObject(x = 0, y = 0) {
+    return {
+        x: x,
+        y: y
+    };
+}
+
+main();
